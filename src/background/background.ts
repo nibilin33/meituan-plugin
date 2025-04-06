082371429bc1d20af1bb54f2b89d8b3c0e069793
nibilin33 <nibilin33@gmail.com>
2025-04-06 12:47:32 +0800

    feat: update readme

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,9 @@
 console.log("Hello world from bakground script");
+
+chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.type === "SHOP_STATS") {
+      console.log("商家数据：", msg.data);
+      chrome.storage.local.set({ todayStats: msg.data });
+      // 或者发送到你的后端 API
+    }
+});
